b711292d21ecff1c8b7721e407218c6f18b98540
varianmj <varianjatnika@gmail.com>
2020-01-07 22:46:32 +0700

    yang pertama di form

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import Header from './Header'
-import Form from './components/FormStudent'
-import ListStudent from './components/ListStudent'
-
-
-// 1. Component
-// 2. State
-// 3. Props
 class App extends Component {
-  constructor(){
-    super()
-    this.state={
-        students : [
-            { name : 'varian', age: 21}
-        ],
-    }
-  }
-
-  addStudent(name,age){
-    let currentStudents = this.state.students
-    let newStudent = { name,age}
-
-    currentStudents.push(newStudent)
-    this.setState({
-        students: currentStudents
-
-    })
-}
   render(){
-  return (
-    <div className="App">
-      <Header/>
-      <Form addStudent= { (name,age)=> this.addStudent}/>
-      <h3> Students: </h3>
-      <ListStudent students={this.state.students}/>
-  
-    </div>
-  );
-}}
+    return <div className="wrapper">
+      <div className="form-wrapper">
+        <hi>Create Account</hi>
+        <form onsubmit={this.handlesubmit} noValidate>
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text"
+             className=""
+             placeholder="First Name" 
+             type="text" name="firstName"
+             noValidate
+             onChange={this.handleChange}
+            />
+          </div>
+          <div className="lastName">
+            <label htmlFor="lastName">last Name</label>
+            <input type="text"
+             className=""
+             placeholder="last Name" 
+             type="text" name="lastName"
+             noValidate
+             onChange={this.handleChange}
+            />
+          </div>
+          <div className="email">
+            <label htmlFor="email">email</label>
+            <input type="text"
+             className=""
+             placeholder="email" 
+             type="text" name="email"
+             noValidate
+             onChange={this.handleChange}
+            />
+          </div>
+          <div className="password">
+            <label htmlFor="password">password</label>
+            <input type="text"
+             className=""
+             placeholder="password" 
+             type="text" name="password"
+             noValidate
+             onChange={this.handleChange}
+            />
+          </div>
+          <div className="createAccount">
+            <button type="submit">Create Account</button>
+            <smal>Already Have an Account?</smal>
+          </div>
+        </form>
+      </div>
+     </div>
 
-
+    
+  }
+}
 export default App;
